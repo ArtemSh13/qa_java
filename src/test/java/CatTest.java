f3b29bea57ac217133921a67bced555dc6bc836d
ArtemSh13 <mozg1303@ya.ru>
2022-05-08 18:18:07 +0300

    Add test for Cat.getFood()

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,11 +3,20 @@ import com.example.Feline;
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.util.List;
+
 public class CatTest {
     @Test
     public void getSoundCatMeow() {
         Cat cat = new Cat(new Feline());
         String expectedString = "Мяу";
         Assert.assertEquals("The cat doesn't say meow", expectedString, cat.getSound());
+    }
+
+    @Test
+    public void getFoodCatListOfAnimalsBirdsFish() throws Exception {
+        Cat cat = new Cat(new Feline());
+        List<String> expectedList = List.of("Животные", "Птицы", "Рыба");
+        Assert.assertEquals("Invalid content of the list", expectedList, cat.getFood());
     }
 }
