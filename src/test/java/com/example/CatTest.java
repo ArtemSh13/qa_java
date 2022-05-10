f63b2c0e53d32424ea306908959e1d29233c3814
ArtemSh13 <mozg1303@ya.ru>
2022-05-10 17:52:15 +0300

    Use Feline mock for Cat test

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,21 +1,33 @@
 package com.example;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.Assert;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import java.util.List;
 
 public class CatTest {
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    Feline felineMock;
+
     @Test
     public void getSoundCatMeow() {
-        Cat cat = new Cat(new Feline());
+        Cat cat = new Cat(felineMock);
         String expectedString = "Мяу";
         Assert.assertEquals("The cat doesn't say meow", expectedString, cat.getSound());
     }
 
     @Test
     public void getFoodCatListOfAnimalsBirdsFish() throws Exception {
-        Cat cat = new Cat(new Feline());
+        Mockito.when(felineMock.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        Cat cat = new Cat(felineMock);
         List<String> expectedList = List.of("Животные", "Птицы", "Рыба");
         Assert.assertEquals("Invalid content of the list", expectedList, cat.getFood());
     }
